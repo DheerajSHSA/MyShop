31c54fb3a4cfe6af5592ecbe5a4bcb3acdfd8842
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2022-12-28 16:39:44 +0530

    Added the routerLink

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { AdminPanelComponent } from './admin-panel/admin-panel.component';
 import { LoginComponent } from './login/login.component';
 import { LogoutComponent } from './logout/logout.component';
 import { MainComponent } from './main/main.component';
+import { NewItemComponent } from './new-item/new-item.component';
 import { OwnerPanelComponent } from './owner-panel/owner-panel.component';
 import { SignupComponent } from './signup/signup.component';
 
@@ -13,7 +14,8 @@ const routes: Routes = [
   { path: 'logout', component: LogoutComponent },
   { path: 'shopping', component: MainComponent },
   { path: 'ownerpanel', component: OwnerPanelComponent },
-  { path: 'adminpanel', component: AdminPanelComponent }
+  { path: 'adminpanel', component: AdminPanelComponent },
+  { path: 'ownerpanel/new-item', component: NewItemComponent }
 ];
 
 @NgModule({
--- src/app/owner-panel/owner-panel.component.ts
+++ src/app/owner-panel/owner-panel.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-owner-panel',
@@ -7,9 +8,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OwnerPanelComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
   ngOnInit(): void {
+  }
+
+  addNewItem(): void { 
+    this.router.navigate(['ownerpanel/new-item'])
   }
 
 }
