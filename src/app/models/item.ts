dfa220ebaee6187a2cfea9ba0079487195d0a94e
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-10 22:53:47 +0530

    Adding the Stock System to the Product List

--- src/app/models/item.ts
+++ src/app/models/item.ts
@@ -2,6 +2,7 @@ export class Item {
     name: string = ''
     imageLink: string = ''
     price: number = 0
+    inStock: boolean = true
 
     public constructor(name: string, imageLink: string, price: number) {
         this.name = name;
