e3c7a970a79f28178919e8850da0b91b0b385177
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-10 22:07:26 +0530

    Added the Method for Removing the Item

--- src/app/components/remove-an-item/remove-an-item.component.ts
+++ src/app/components/remove-an-item/remove-an-item.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Item } from 'src/app/models/item';
+import { ItemsService } from 'src/app/services/items.service';
 
 @Component({
   selector: 'app-remove-an-item',
@@ -7,9 +9,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RemoveAnItemComponent implements OnInit {
 
-  constructor() { }
+  constructor(public itemService: ItemsService) { }
 
   ngOnInit(): void {
+  }
+
+  // Function made for onClick event of the Remove Button on the Page
+  removeItem(item: Item) {
+    this.itemService.removeItem(item);
   }
 
 }
--- src/app/services/items.service.ts
+++ src/app/services/items.service.ts
@@ -5,8 +5,16 @@ import { Item } from '../models/item';
   providedIn: 'root'
 })
 export class ItemsService {
+  // Temporary Database
   items: [Item] = [
+    // Temporary Example for Item
     new Item('Ring', 'https://staticimg.titan.co.in/Tanishq/Catalog/513218FPLAA00_1.jpg', 99)
   ]
   constructor() { }
+
+  // Function to remove the Item from the Database
+  removeItem(itemToRemove: Item): void {
+    this.items.splice(this.items.indexOf(itemToRemove), 1);
+
+  }
 }
