947c9f212cbd751f0e35cbc35772f3a896f7e990
Dheeraj kashyap <Dheerajkashyap4889@gmail.com>
2023-01-12 15:34:09 +0530

    Compelted "Change Name" Page

--- src/app/components/change-name/change-name.component.ts
+++ src/app/components/change-name/change-name.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { HeaderServiceService } from 'src/app/services/header-service.service';
 
 @Component({
   selector: 'app-change-name',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./change-name.component.css']
 })
 export class ChangeNameComponent implements OnInit {
-
-  constructor() { }
+  heading: string = 'Update the Name'
+  newName: string = ''
+  constructor(
+    private router: Router,
+    public headerService: HeaderServiceService
+  ) { }
 
   ngOnInit(): void {
   }
 
+  //Function for navigation to the Main Menu
+  back(): void {
+    this.router.navigate(['ownerpanel'])
+  }
+
+  //Function to invoke the changeName method in the service
+  changeName(name: string): void {
+    this.headerService.changeName(name);
+  }
 }
--- src/app/components/header/header.component.ts
+++ src/app/components/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { HeaderServiceService } from 'src/app/services/header-service.service';
 import { AuthenticationServiceService } from '../../services/authentication-service.service';
 
 @Component({
@@ -7,11 +8,15 @@ import { AuthenticationServiceService } from '../../services/authentication-serv
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
-  constructor(public service: AuthenticationServiceService) { }
+  constructor(
+    public service: AuthenticationServiceService,
+    public headerService: HeaderServiceService
+    ) { }
 
   ngOnInit(): void {
   }
 
+  //Function to invoke the logout method in the Service
   logout() {
     this.service.logout();
   }
